285f2502f314dab67ddf8825d972256f63f86a09
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-14 10:57:37 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@/my_database")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	values := make([]sql.RawBytes, len(columns))
+
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var value string
+		for i, col := range values {
+			if col == nil {
+				value = "NILL"
+			} else {
+				value = string(col)
+			}
+
+			fmt.Println(columns[i], ": ", value)
+		}
+		fmt.Println("-----------------")
+	}
+
+
+}
