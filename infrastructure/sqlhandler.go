f6f75d3206e208511f200a1bc6e1b73a6a5681fb
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-14 15:50:46 +0900

    DIP

--- infrastructure/sqlhandler.go
+++ infrastructure/sqlhandler.go
@@ -3,6 +3,7 @@ package infrastructure
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/ryuseikurata/DDD/interfaces/database"
 )
 
 type SqlHandler struct {
@@ -17,4 +18,52 @@ func NewSqlHandler() *SqlHandler {
 	sqlHandler := new(sqlHandler)
 	sqlHandler.conn = conn
 	return sqlHandler
+}
+
+func (handler *SqlHandler) Execute(statement string, args ...interface{}) ( database.Result, error ) {
+	res := SqlResult{}
+	result, err := handler.Conn.Exec(statement, args...)
+	if err != nil {
+		return res, err
+	}
+	res.Result = result
+	return res, err
+}
+
+func (handler *SqlHandler) Query(statement string, args ...interface{}) (row database.Row, error) {
+	rows, err := handler.Conn.Query(statement, args...)
+	if err != nil {
+		return new(SqlRow), err
+	}
+	row := new(SqlRow)
+	row.Rows = rows
+	return row, nil
+}
+
+type SqlResult struct {
+	Result sql.Result
+}
+
+func (r SqlResult) LastInsertId() (int64, error) {
+	return r.Result.LastInsertId()
+}
+
+func (r SqlResult) RowsAffected() (int64, error) {
+	return r.Result.RowsAffected()
+}
+
+type SqlRow struct {
+	Rows *sql.Rows
+}
+
+func (r SqlRow) Scan(dest ...interface{}) error {
+	return r.Rows.Scan(dest...)
+}
+
+func (r SqlRow) Next() bool {
+	return r.Rows.Next()
+}
+
+func (r SqlRow) Close() error {
+	return r.Rows.Close()
 }
