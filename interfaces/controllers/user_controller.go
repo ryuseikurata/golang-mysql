05ed94a073a89c10af1e50a103b2d3fe18118908
Ryusei Kurata <amnosmusic13@gmail.com>
2019-05-15 12:34:22 +0900

    contorller

--- interfaces/controllers/user_controller.go
+++ interfaces/controllers/user_controller.go
@@ -2,8 +2,9 @@ package controllers
 
 import (
 	"github.com/ryuseikurata/DDD/usecase"
+	"github.com/ryuseikurata/DDD/domain"
 	"github.com/ryuseikurata/DDD/interfaces/database"
-	
+	"strconv"
 )
 
 type UserController struct {
@@ -18,4 +19,34 @@ func NewUserController(sqlHandler database.SqlHandler) *UserController {
 			},
 		},
 	}
+}
+
+func (controller *UserController) Create(c Context) {
+	user := domain.User{}
+	c.Bind(&user)
+	err := controller.Interactor.Add(user)
+	if err != nil {
+		c.JSON(500, NewError(err))
+		return
+	}
+	c.JSON(201, user)
+}
+
+func (controller *UserController) Index(c Context) {
+	users, err := controller.Interactor.Users()
+	if err != nil {
+		c.JSON(500, NewError(err))
+		return 
+	}
+	c.JSON(200, users)
+}
+
+func (controller *UserController) Show(c Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	user, err := controller.Interactor.UserById(id)
+	if err != nil {
+		c.JSON(500, NewError(err))
+		return
+	}
+	c.JSON(200, user)
 }
